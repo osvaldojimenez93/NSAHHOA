4cae5e8eb9f40a2221af51f979560e0a3a0ef0f0
Osvaldo Jimenez <osjimene@microsoft.com>
2024-07-04 14:39:50 -0500

    Initial Commit

---
+++ Home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+# Setting the title of the Streamlit app
+st.title("North San Antonio Hills HOA")
+st.image("assets/HOA.png", use_column_width=True)
+
+
+# Home page
+st.header("Welcome to North San Antonio Hills")
+st.write("""
+The North San Antonio Hills Homeowners Association (NSAH HOA) is dedicated to maintaining and improving the quality of life in our community. Our website is designed to provide important information about our neighborhood, including upcoming events, news, and resources for residents.
+""")
+st.write("""
+## Latest News
+## 🏗️**CURRENTLY UNDER CONSTRUCTION**🚧""")
+
+
+
