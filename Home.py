9e10fb93e9ac30c9b65f2fed52b9c5c8c0b81ba0
Osvaldo Jimenez <osjimene@microsoft.com>
2024-07-24 16:18:05 -0700

    Added new documents and edited links on the landing page

--- Home.py
+++ Home.py
@@ -12,13 +12,26 @@ st.header("Welcome to North San Antonio Hills")
 st.write("""
 The North San Antonio Hills Homeowners Association (NSAH HOA) is dedicated to maintaining and improving the quality of life in our community. Our website is designed to provide important information about our neighborhood, including upcoming events, news, and resources for residents.
 """)
-st.write("""
-## Latest News
-""")
 
 st.write("""
 ## Latest News
 - [Read our most recent newsletter](https://nsah-hoa.streamlit.app/Community_News)
+""")
+
+st.write("""
+## Contact Collection Form
+Help us keep in touch with you by providing your contact information. This will allow us to send you important updates and information about our community.
+- [Contact collection form](https://forms.office.com/r/kG3HjSEWW1)
+""")
+
+st.write("""
+## Helpful Community Resources
+- [District 6 City Council](https://www.sa.gov/Directory/Departments/Mayor-Council/City-Council/D6)
+- [SAWS Website](https://www.saws.org/)
+- [CPS Energy Website](https://cpsenergy.com/)
+- [Garbage Collection Map](https://gis.sanantonio.gov/swmd/mycollectionday/default.html)
+- [Bexar County Propery Maps](https://bexar.trueautomation.com/mapSearch/?cid=110)
+- [City of San Antonio Website](https://www.sanantonio.gov/)
 """)
 
 st.write("""
--- pages/03_Documents.py
+++ pages/03_Documents.py
@@ -7,7 +7,30 @@ st.set_page_config(layout="wide", page_title="North San Antonio Hills HOA")
 st.markdown("<h1 style='text-align: center;'>North San Antonio Hills HOA</h1>", unsafe_allow_html=True)
 st.image("assets/HOA.png", use_column_width=True)
 
-st.header("HOA Documents")
+covenants = "documents/Restrictive Covenants.pdf"
+with open(covenants, "rb") as f:
+    covenant_bytes = f.read()
+acc = "documents/ACC Renewal.pdf"
+with open(acc, "rb") as f:
+    acc_bytes = f.read()
+
+# st.header("HOA Documents")
+st.write("""
+## Governing Documents:
+""")
+st.download_button(
+    label="Download Restrictive Covenants",
+    data=covenant_bytes,
+    file_name="Restrictive Covenants.pdf",
+    mime="application/pdf"
+)
+st.download_button(
+    label="Download ACC Renewal Document",
+    data=acc_bytes,
+    file_name="ACC Renewal Document.pdf",
+    mime="application/pdf"
+)
+
 st.write("""
 ## 🏗️**CURRENTLY UNDER CONSTRUCTION**🚧
 """)
