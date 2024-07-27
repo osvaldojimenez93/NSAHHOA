0d8b4383330df0a272ffed12b012bfcbc6a0bfb8
Osvaldo Jimenez <osjimene@microsoft.com>
2024-07-27 09:43:23 -0700

    Changed font, added calendar, changed about us to committees page

--- pages/01_Events.py
+++ pages/01_Events.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import streamlit.components.v1 as components
 
 # Setting the title of the Streamlit app
 st.set_page_config(layout="wide", page_title="North San Antonio Hills HOA")
@@ -10,7 +11,9 @@ st.image("assets/HOA.png", use_column_width=True)
 st.header("Upcoming Events")
 st.write("""
 Join us for our upcoming events! We have a variety of activities planned for residents to enjoy and participate in. Mark your calendars and join the fun!
-""")
-st.write("""
-## 🏗️**CURRENTLY UNDER CONSTRUCTION**🚧
-""")
+
+If you would like an event to be added to this calendar, please email us [Here](mailto:assistantsecretary@nsah-hoa.org).""")
+
+components.html(f"""
+<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&bgcolor=%23ffffff&title=North%20San%20Antonio%20Hills&showCalendars=0&showTabs=0&src=OTU0NDI4ZjhhNjFiOTkxOWQwOWM5YzdhOTFmYmU4OTZjNjU5NzYxYjc3ZjhmNzJhYjhkNWMxNWJkZThhZjJhOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23A79B8E&color=%230B8043" style="border:solid 1px #777" width="100%" height="600" frameborder="0" scrolling="no"></iframe>""",
+                height=600)
---
+++ pages/04_Committees.py
@@ -0,0 +1,72 @@
+import streamlit as st
+
+# Setting the title of the Streamlit app
+st.set_page_config(layout="wide", page_title="North San Antonio Hills HOA")
+
+# Setting the title of the Streamlit app
+st.markdown("<h1 style='text-align: center;'>North San Antonio Hills HOA</h1>", unsafe_allow_html=True)
+st.image("assets/HOA.png", use_column_width=True)
+
+st.header("North San Antonio Hills HOA Committees")
+st.write("""
+   In order to better serve the community and address the needs of our residents as a team, 
+         the North San Antonio Hills HOA has established several committees led by board members or members at large.
+         If you are interested in joining a committee, please contact the committee chairperson directly.
+      
+   If you have an idea for a new committee, please contact the HOA board [Here](mailto:president@nsah-hoa.org;vicepresident@nsah-hoa.org;secretary@nsah-hoa.org;assistantsecretary@nsah-hoa.org).""")
+
+# Committees data
+committees = {
+   "City - Community Relations": [
+      "Jessica Grigsby",
+      "Davin Dukes",
+      "Cole Hudson",
+      "Osvaldo 'Jim' Jimenez"
+   ],
+   "Neighborhood - Community Relations": [
+      "Davin Dukes",
+      "Osvaldo 'Jim' Jimenez",
+      "Kevin Mantovani",
+      "Alan Tuma"
+   ],
+   "Budget & Finance Committee": [
+      "Jessica Grigsby",
+      "Mike Dammonn",
+      "Cole Hudson"
+   ],
+   "HOA Website - FB - IG": [
+      "Osvaldo Jimenez",
+      "Greg Jones"
+   ],
+   "Area Development/Realestate": [
+      "Jessica Grigsby",
+      "Cole Hudson",
+      "Richard Ramos",
+      "Linda Pilar"
+   ],
+   "Social Events Coordinator": [
+      "Looking for volunteers!"
+   ],
+   "Park Maintenance/Clean Up": [
+      "Looking for volunteers!"
+   ],
+   "Sign Maintenance": [
+      "Looking for volunteers!"
+   ],
+   "Architectural": [
+      "Miguel Sanchez",
+      "Thomas",
+      "Jessica Grigsby"
+   ]
+}
+
+# Create a table of contents with hyperlinks
+st.markdown("## Table of Contents")
+for committee in committees.keys():
+   st.markdown(f"- [{committee}](#{committee.replace(' ', '-').lower().replace('--', '-')})")
+
+# Create sections for each committee
+for committee, members in committees.items():
+   st.markdown(f"### {committee}")
+   for member in members:
+      st.markdown(f"- {member}")
