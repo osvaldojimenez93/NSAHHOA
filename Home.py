d735606143488489cb7d21a2d3ef96e24f3ba60d
Osvaldo 'Jim' Jimenez 🤠 <osjimene@microsoft.com>
2025-05-02 09:27:02 -0500

    added annual meeting announcement

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ render_page_template("Welcome to North San Antonio Hills")
 st.write("""
 The North San Antonio Hills Homeowners Association (NSAH HOA) is dedicated to maintaining and improving the quality of life in our community. Our website is designed to provide important information about our neighborhood, including upcoming events, news, and resources for residents.
 """)
-
+st.image("assets/Annual Meeting.png", use_container_width=True)
 st.write("""
 ## Latest News
 """)
