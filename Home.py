9d1c109b1be726bc143b2a743b6455f07740704f
Osvaldo 'Jim' Jimenez 🤠 <osjimene@microsoft.com>
2025-08-05 16:57:08 -0500

    changed to script instead of html

--- Home.py
+++ Home.py
@@ -4,12 +4,13 @@ import streamlit as st
 # Replace with your new Vercel URL
 new_url = "https://nsah-hoa.org/"
 
+
 st.markdown(f"""
-    <meta http-equiv="refresh" content="0; url={new_url}" />
+    <script>
+        window.location.replace("{new_url}");
+    </script>
     If you are not redirected automatically, <a href="{new_url}">click here</a>.
 """, unsafe_allow_html=True)
-
-
 
 # Prevent indexing
 st.markdown("""
