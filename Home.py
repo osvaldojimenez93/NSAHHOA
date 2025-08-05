c284834600afc84c957b8048a8e4cb4efe64a330
Osvaldo 'Jim' Jimenez 🤠 <osjimene@microsoft.com>
2025-08-05 16:40:43 -0500

    prevent indexing.

--- Home.py
+++ Home.py
@@ -8,3 +8,10 @@ st.markdown(f"""
     <meta http-equiv="refresh" content="0; url={new_url}" />
     If you are not redirected automatically, <a href="{new_url}">click here</a>.
 """, unsafe_allow_html=True)
+
+
+
+# Prevent indexing
+st.markdown("""
+    <meta name="robots" content="noindex">
+""", unsafe_allow_html=True)
