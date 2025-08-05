5eb76d2e848986383ba3b1da6a018522b27f78d9
Osvaldo 'Jim' Jimenez 🤠 <osjimene@microsoft.com>
2025-08-05 16:38:25 -0500

    set redirect to new website.

--- Home.py
+++ Home.py
@@ -1,44 +1,10 @@
+
 import streamlit as st
-from components.PageTemplate import render_page_template
-import streamlit.components.v1 as components
 
-render_page_template("Welcome to North San Antonio Hills")
+# Replace with your new Vercel URL
+new_url = "https://nsah-hoa.org/"
 
-
-st.write("""
-The North San Antonio Hills Homeowners Association (NSAH HOA) is dedicated to maintaining and improving the quality of life in our community. Our website is designed to provide important information about our neighborhood, including upcoming events, news, and resources for residents.
-""")
-st.image("assets/Annual Meeting.png", use_container_width=True)
-st.write("""
-## Latest News
-""")
-
-with st.container():
-    components.html("""
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fprofile.php%3Fid%3D61564446704590%26mibextid%3DLQQJ4d&tabs=timeline&width=1200&height=600&small_header=true&adapt_container_width=false&hide_cover=true&show_facepile=false&appId" 
-        width="1200" height="600" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" 
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
-    """, width=500, height=600)
-
-
-
-st.write("""
-## Community Information Collection Forms
-- [Feedback collection form](https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAABxo201URFowOVc0NzZTVUVGSThDTTRPSzhJRVhXTC4u) - Give the board feedback/ideas for improvement.
-- [Contact collection form](https://forms.office.com/r/kG3HjSEWW1) - Help us keep in touch with you by providing your contact information. This will allow us to send you important updates and information about our community.
-""")
-
-st.write("""
-## Helpful Community Resources
-- [District 6 City Council](https://www.sa.gov/Directory/Departments/Mayor-Council/City-Council/D6)
-- [SAWS Website](https://www.saws.org/)
-- [CPS Energy Website](https://cpsenergy.com/)
-- [Garbage Collection Map](https://gis.sanantonio.gov/swmd/mycollectionday/default.html)
-- [Bexar County Propery Maps](https://bexar.trueautomation.com/mapSearch/?cid=110)
-- [City of San Antonio Website](https://www.sanantonio.gov/)
-""")
-
-
-
-
-
+st.markdown(f"""
+    <meta http-equiv="refresh" content="0; url={new_url}" />
+    If you are not redirected automatically, <a href="{new_url}">click here</a>.
+""", unsafe_allow_html=True)
